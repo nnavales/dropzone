ffe2fc3a639130aa8801390ee7f0f8dccf554ca4
Nahuel Navales <navalesnahuel@gmail.com>
2026-09-23 16:50:33 -0300

    logger: simplified the logger package

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -6,28 +6,12 @@ import (
 	"log/slog"
 )
 
-type Format string
-
-const FormatJSON Format = "json"
-const FormatText Format = "text"
-
-// New creates a new logger object.
-func New(output io.Writer, format Format) *slog.Logger {
-
+// New creates a new logger.
+func New(output io.Writer, level slog.Level, addSource bool) *slog.Logger {
 	opts := &slog.HandlerOptions{
-		AddSource: true,
-		Level:     slog.LevelDebug,
+		Level:     level,
+		AddSource: addSource,
 	}
 
-	var handler slog.Handler
-	switch format {
-	case FormatJSON:
-		handler = slog.NewJSONHandler(output, opts)
-	case FormatText:
-		handler = slog.NewTextHandler(output, opts)
-	default:
-		handler = slog.NewTextHandler(output, opts)
-	}
-
-	return slog.New(handler)
+	return slog.New(slog.NewTextHandler(output, opts))
 }
